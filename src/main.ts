21647b2d850c74ae9d601756b433675ff405a227
Luciano <sobral.luciano@gmail.com>
2021-04-06 22:24:43 -0400

    fix: configuracao dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import express from "express";
 import routes from "./routes";
-import * as dotenv from "dotenv";
-dotenv.config({ path: __dirname + "/.env" });
 
 const app = express();
 app.use(express.json());
--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 import { Address } from "./domain/address";
 import { Coordinate } from "./domain/coordinate";
 import { CalculateDistanceBetweenAddresses } from "./usecase/calculateDistanceBetweenAddresses";
+require("dotenv").config();
 
 const routes = express();
 
